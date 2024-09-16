9df96110d9cdacba7d2e1094eedf1cc10fad1882
Jongwon Kim <growjong8802@gmail.com>
2024-09-17 00:22:16 +0900

    practice : 개발자 유미 스프링 배치 5 : 6. 배치 처리1 : 테이블 to 테이블 BeforeEntity, AfterEntity를 만들고 각 Repository를 JpaRepository를 상속받은 인터페이스로 생성 FirstBatch를 생성하고 Configuration어노테이션으로 등록 후 배치 작업 소스코드를 작성함 JobRepository(JobLauncher - Job - Step) - itemReader, ItemProcessor, ItemWriter

---
+++ src/main/java/com/jongwon/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.jongwon.repository;
+
+import com.jongwon.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ src/main/java/com/jongwon/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package com.jongwon.repository;
+
+import com.jongwon.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
