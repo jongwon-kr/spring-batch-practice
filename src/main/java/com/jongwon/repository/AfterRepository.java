d57f33326c878e787232395ae6d229c8819b715a
Jongwon Kim <growjong8802@gmail.com>
2024-09-17 10:56:03 +0900

    refactor : @Repository 어노테이션 추가

--- src/main/java/com/jongwon/repository/AfterRepository.java
+++ src/main/java/com/jongwon/repository/AfterRepository.java
@@ -2,6 +2,8 @@ package com.jongwon.repository;
 
 import com.jongwon.entity.AfterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
 }
